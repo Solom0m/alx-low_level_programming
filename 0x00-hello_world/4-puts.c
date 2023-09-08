d6e93445b4297b2a1e96b4c136a762c15da469e5
Solom0m <solomonnnamani01@gmail.com>
2023-09-08 12:44:13 +0000

    Alll done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle'\n");
-return 0
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
