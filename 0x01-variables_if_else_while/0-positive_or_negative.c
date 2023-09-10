66af3f209d4e21e0280d616c1c9ca4592d999e37
Solom0m <solomonnnamani01@gmail.com>
2023-09-09 17:41:50 -0700

    my first if else code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main: main function
- * Return: 0;
+ * Print a random number and check whether it is positive,negative or zero.
+ * Return: Always  0
  */
 int main(void)
 {
@@ -9,15 +11,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
 	if(n > 0){
 		printf("%d is positive\n", n);
 	}
-	else if( n == 0){
-		printf("%d is zero\n", n);
+	else if(n < 0){
+		printf("%d is negative\n", n);
 	}
 	else{
-	printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 	}
 		return (0);
 }
