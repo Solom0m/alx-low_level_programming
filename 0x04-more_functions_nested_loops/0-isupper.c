0b987e805138486db4c62618faeea3218826f2aa
Solom0m <solomonnnamani01@gmail.com>
2023-09-15 19:23:27 -0700

    God abeg

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,17 @@
 
 /**
  * _isupper - uppercase letters
- *
  * @c: character to check
+ *
  * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
- 		{
+{
 		return (1);
-		}
+}
 		return (0);
+
 }
